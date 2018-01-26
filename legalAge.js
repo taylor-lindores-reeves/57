b3ae72c02568f448a93e1a27065bd7cacee678df
leafyshark <taylor-lindores@hotmail.co.uk>
2018-01-26 13:27:03 +0000

    Completed legal driving age. Next is blood alcohol calculator.

---
+++ legalAge.js
@@ -0,0 +1,26 @@
+var age = document.getElementById('age');
+var display = document.getElementById('display');
+
+var ages = {
+    'south africa': 17,
+    'nigeria': 23,
+    'canada': 14,
+    'united states': 14,
+    'iraq': 17,
+    'india': 18,
+    'united kingdom': 17,
+    'indonesia': 17,
+    'china': 18,
+    'el salvador': 15,
+    'estonia': 16
+}
+
+age.addEventListener("input", function() {
+    display.innerHTML = ``;
+    for (i in ages) {
+        if (age.value == ages[i]) {
+            display.innerHTML += `<li>${i}</li>`
+        }
+    }
+})
+
